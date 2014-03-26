b0b57db4fd03cd4a1ae06dc97ed510b5a8ea2bbd
LeOSW42 <leo-serre@legtux.org>
2014-03-26 21:16:53 +0100

    Adding license overlay

--- working_on/js.js
+++ working_on/js.js
@@ -5,3 +5,20 @@ $( "#copyleft" ).stop().hover(function() {
 $( "#copyleft" ).stop().mouseleave(function() {
 	$( "#copyleft" ).animate({opacity: 0.4}, 'fast');
 });
+
+$( "#copyleft" ).stop().click(function() {
+	$( "#overlay" ).fadeIn();
+});
+
+$('#overlay').on('click', function (e) {
+    var $parent = $('#overlay'),
+        $eventTarget = $(e.target);
+    
+    if ($parent.has($eventTarget).length === 0) {
+        $( "#overlay" ).fadeOut('fast');
+    }
+});
+
+$( document.body ).stop().keypress(function() {
+	$( "#overlay" ).fadeOut('fast');
+});
