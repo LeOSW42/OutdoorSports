54fa71f8c1ec7aaf3d63083edc1342c9328782e8
LéOSW <leo-serre@legtux.org>
2014-04-01 22:23:38 +0200

    Misspelling

--- include/pages/home.php
+++ include/pages/home.php
@@ -1,8 +1,7 @@
-
 <blockquote>Outdoor Sports est un projet de centralisation des informations utiles aux passionné(e)s de sports d'aventure, tels la randonnée, l'alpinisme, le cyclisme, l'escalade, etc.
 On y trouve des guides de randonnées actualisés et libres de droits, des informations sur les refuges et des annonces de covoiturage, le tout orienté vers les besoins du montagnard.<br />
 Ce projet est né lors d'une forte discussion enivrée sur le futur de l'économie.
-C'est donc aussi une expérience personelle que de voir si au long terme, il est possible de vivre du financement participatif à petite échelle.</blockquote>
+C'est donc aussi une expérience personnelle que de voir si au long terme, il est possible de vivre du financement participatif à petite échelle.</blockquote>
 
 <div id="home_credits">
 	<p>Propulsé par</p>
