748f2e9e188a5bf350e30ebf753c7d83391e410d
LeOSW42 <leo-serre@legtux.org>
2014-04-10 20:17:56 +0200

    Some buttons position corrects (fullscreen, moving header)

--- include/pages/guides_list.php
+++ include/pages/guides_list.php
@@ -2,34 +2,11 @@
 
 	<div id="map"></div>
 	<div id="list">
-		Sed cautela nimia in peiores haeserat plagas, ut narrabimus postea, aemulis consarcinantibus insidias graves apud Constantium, cetera medium principem sed siquid auribus eius huius modi quivis infudisset ignotus, acerbum et inplacabilem et in hoc causarum titulo dissimilem sui.
-
-		Quaestione igitur per multiplices dilatata fortunas cum ambigerentur quaedam, non nulla levius actitata constaret, post multorum clades Apollinares ambo pater et filius in exilium acti cum ad locum Crateras nomine pervenissent, villam scilicet suam quae ab Antiochia vicensimo et quarto disiungitur lapide, ut mandatum est, fractis cruribus occiduntur.
-
-		Haec subinde Constantius audiens et quaedam referente Thalassio doctus, quem eum odisse iam conpererat lege communi, scribens ad Caesarem blandius adiumenta paulatim illi subtraxit, sollicitari se simulans ne, uti est militare otium fere tumultuosum, in eius perniciem conspiraret, solisque scholis iussit esse contentum palatinis et protectorum cum Scutariis et Gentilibus, et mandabat Domitiano, ex comite largitionum, praefecto ut cum in Syriam venerit, Gallum, quem crebro acciverat, ad Italiam properare blande hortaretur et verecunde.
-
-		Utque aegrum corpus quassari etiam levibus solet offensis, ita animus eius angustus et tener, quicquid increpuisset, ad salutis suae dispendium existimans factum aut cogitatum, insontium caedibus fecit victoriam luctuosam.
-
-		Haec igitur lex in amicitia sanciatur, ut neque rogemus res turpes nec faciamus rogati. Turpis enim excusatio est et minime accipienda cum in ceteris peccatis, tum si quis contra rem publicam se amici causa fecisse fateatur. Etenim eo loco, Fanni et Scaevola, locati sumus ut nos longe prospicere oporteat futuros casus rei publicae. Deflexit iam aliquantum de spatio curriculoque consuetudo maiorum.
-
-		<a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br><a href="?/guides/guide">Example</a><br>
-
-		Haec igitur Epicuri non probo, inquam. De cetero vellem equidem aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis. quamquam te quidem video minime esse deterritum.
+		<p>Petit descriptif de list</p><br>
+		<a href="?/guides/guide">Exemple de randonnée</a>
 	</div>
 	<div id="search">
-		Sed cautela nimia in peiores haeserat plagas, ut narrabimus postea, aemulis consarcinantibus insidias graves apud Constantium, cetera medium principem sed siquid auribus eius huius modi quivis infudisset ignotus, acerbum et inplacabilem et in hoc causarum titulo dissimilem sui.
-
-		Quaestione igitur per multiplices dilatata fortunas cum ambigerentur quaedam, non nulla levius actitata constaret, post multorum clades Apollinares ambo pater et filius in exilium acti cum ad locum Crateras nomine pervenissent, villam scilicet suam quae ab Antiochia vicensimo et quarto disiungitur lapide, ut mandatum est, fractis cruribus occiduntur.
-
-		Haec subinde Constantius audiens et quaedam referente Thalassio doctus, quem eum odisse iam conpererat lege communi, scribens ad Caesarem blandius adiumenta paulatim illi subtraxit, sollicitari se simulans ne, uti est militare otium fere tumultuosum, in eius perniciem conspiraret, solisque scholis iussit esse contentum palatinis et protectorum cum Scutariis et Gentilibus, et mandabat Domitiano, ex comite largitionum, praefecto ut cum in Syriam venerit, Gallum, quem crebro acciverat, ad Italiam properare blande hortaretur et verecunde.
-
-		Utque aegrum corpus quassari etiam levibus solet offensis, ita animus eius angustus et tener, quicquid increpuisset, ad salutis suae dispendium existimans factum aut cogitatum, insontium caedibus fecit victoriam luctuosam.
-<br><br><a href="http://wxs.ign.fr/mvyjs9blkl0qe94kg23zmxtf/geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS&FORMAT=image%2Fjpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&&TILEMATRIX=13&TILECOL=4096&TILEROW=2723">Toto</a><br>
-		Haec igitur lex in amicitia sanciatur, ut neque rogemus res turpes nec faciamus rogati. Turpis enim excusatio est et minime accipienda cum in ceteris peccatis, tum si quis contra rem publicam se amici causa fecisse fateatur. Etenim eo loco, Fanni et Scaevola, locati sumus ut nos longe prospicere oporteat futuros casus rei publicae. Deflexit iam aliquantum de spatio curriculoque consuetudo maiorum.
-
-		Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.
-
-		Haec igitur Epicuri non probo, inquam. De cetero vellem equidem aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis. quamquam te quidem video minime esse deterritum.
+		<p>Non implémenté</p>		
 	</div>
 
 <script type="text/javascript">
@@ -79,6 +56,38 @@ $(window).resize(function() {
 	};
 	map.invalidateSize();
 }).trigger("resize");
+
+$('#arrow').click(function() {
+	if(headerPresent==1) {
+		$("#map").animate({height: $("body").height()-41}, 300);
+		$(".leaflet-control-container .leaflet-top.leaflet-right").animate({marginTop: 80}, 300);
+		$("#buttons").animate({top: 50}, 300);
+	}
+	else {
+		$("#map").animate({height: $("body").height()-$("header").height()-41}, 300);
+		$(".leaflet-control-container .leaflet-top.leaflet-right").animate({marginTop: 60}, 300);
+		$("#buttons").animate({top: 30}, 300);
+	}
+	map.invalidateSize();
+});
+
+map.on('enterFullscreen', function(){
+	$(".leaflet-control-container .leaflet-top").css({marginTop: 5});
+});
+
+map.on('exitFullscreen', function(){
+	if(headerPresent==1) {
+		$("#map").css("height", $("body").height()-$("header").height()-41);
+		$(".leaflet-control-container .leaflet-top.leaflet-right").css({marginTop: 60});
+		$(".leaflet-control-container .leaflet-top.leaflet-left").css({marginTop: 30});
+	}
+	else {
+		$("#map").css("height", $("body").height()-41);
+		$(".leaflet-control-container .leaflet-top.leaflet-right").css({marginTop: 80}, 300);
+		$(".leaflet-control-container .leaflet-top.leaflet-left").css({marginTop: 30});
+	}
+	map.invalidateSize();
+});
 
 // Page layers
 
